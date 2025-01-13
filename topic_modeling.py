e7a7b8badaf50aa5763dba6e7344683b47b14784
Fedor Vitiugin <fedor.vitiugin@aalto.fi>
2025-01-13 13:41:14 +0200

    RnR paper updates

--- topic_modeling.py
+++ topic_modeling.py
@@ -7,11 +7,7 @@ from umap import UMAP
 from sklearn.feature_extraction.text import CountVectorizer
 from sentence_transformers import SentenceTransformer
 
-from transformers.pipelines import pipeline
 from bertopic.representation import MaximalMarginalRelevance
-
-from bertopic import BERTopic
-import pandas as pd
 
 from umap import UMAP
 from bertopic import BERTopic
@@ -39,24 +35,21 @@ embeddings = sentence_model.encode(docs, show_progress_bar=True)
 seed_topic_list = [['Bias', 'Discrimination', 'Non-discrimination', 'Fair', 'Fairness', 'Exclusion', 'Hate speech'], # Discrimination, hate speech, and exclusion
             ['Personal information', 'Sensitive information', 'Private', 'Privacy', 'Sensitive data', 'Personal data'], # Privacy and personal information
             ['Attack', 'Cyber', 'Cybersecurity', 'Malicious', 'Adversarial'], # Cyber threats and malicious use
-            ['Misinformation','Disinformation','Fake news'], # Mis- and disinformation
-            ['Environmental','Environment','Sustainable','Sustainability','Climate','Ecological'], # Environment and sustainability
+            ['Misinformation', 'Disinformation', 'Fake news'], # Mis- and disinformation
+            ['Environmental', 'Environment', 'Sustainable', 'Sustainability', 'Climate', 'Ecological'], # Environment and sustainability
             ['Labor', 'Job', 'Work', 'Workforce', 'Company', 'Public service', 'Public sector']] # Labor market
 
-mmr_model = MaximalMarginalRelevance(diversity=0.2)
-
 vectorizer_model = CountVectorizer(stop_words="english", ngram_range=(1, 2))
-representation_model = {"MMR": mmr_model}
 
-umap_model = UMAP(n_neighbors=2, n_components=10, min_dist=0.4, metric='cosine', random_state=13) # https://maartengr.github.io/BERTopic/getting_started/dim_reduction/dim_reduction.html
+umap_model = UMAP(n_neighbors=5, n_components=5, min_dist=0.6, metric='cosine', random_state=13) # https://maartengr.github.io/BERTopic/getting_started/dim_reduction/dim_reduction.html
 
-topic_model = BERTopic(nr_topics=len(seed_topic_list), umap_model=umap_model, representation_model=representation_model, vectorizer_model=vectorizer_model, 
+topic_model = BERTopic(nr_topics=len(seed_topic_list), umap_model=umap_model, vectorizer_model=vectorizer_model, 
                        seed_topic_list=seed_topic_list, verbose=True, calculate_probabilities=True)
 
-topics, probs = topic_model.fit_transform(docs, embeddings,)
-df['topic'] = topics
+topics, probs = topic_model.fit_transform(docs, embeddings)
+topic_model.get_topic_info()
 
-print()
+df['topic'] = topics
 
 # save in dataset file
 df.to_csv(path_to_data_folder + '_with_topics.csv', index=False)
